7aec60a4f6d375a0edc6220db382de058d1254b9
Mher <tools@logreg.e4ward.com>
2021-04-29 14:23:17 +0400

    Fix script to select all dropdown togglers
    Use querySelectorAll to get a NodeList and iterate using forEach or
    Array.prototype.forEach.call()

--- 10-pattern-library/src/scripts/docs.js
+++ 10-pattern-library/src/scripts/docs.js
@@ -1,9 +1,10 @@
 
 (function() {
-  const toggler = document.querySelector('.dropdown__toggle');
-  toggler.addEventListener('click', function (evt) {
-    // const dropdown = document.querySelector('.dropdown');
-    const dropdown = evt.target.parentNode;
-    dropdown.classList.toggle('is-open');
+  const togglers = document.querySelectorAll('.dropdown__toggle');
+  Array.prototype.forEach.call(togglers, function (toggler) {
+    toggler.addEventListener('click', function (evt) {
+      const dropdown = evt.target.parentNode;
+      dropdown.classList.toggle('is-open');
+    })
   })
 })()
