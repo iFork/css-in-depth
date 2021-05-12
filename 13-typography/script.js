ad07ea835099a3a739ee0f5564593e96636e845a
Mher <tools@logreg.e4ward.com>
2021-05-12 17:30:35 +0400

    Failover font w/ observer, fix FOIT>FOUT>styled
    Set initial wfonts-inactive to prevent even a breif FOIT
    because both in Chrome and Firefox, even w/o initially setting
    .wfonts-inactive but waiting for a timeout and setting .fonts-failed
    only after failure I *CANNOT produce full FOIT effect till the end of
    fontobservers timeout*.
    In reality, w/o `font-display` descriptor or `font-display: block` I get
    sequence of:
    FOIT > FOUT > styled text.
    
    Now there is no any FOIT, only FOUT with specified failover font.

--- 13-typography/script.js
+++ 13-typography/script.js
@@ -1,5 +1,15 @@
 
 const html = document.documentElement;
+/* 
+NOTE: setting initial wfonts-inactive prevents even a breif FOIT
+because both in Chrome and Firefox, even w/o initially setting .wfonts-inactive
+but waiting for a timeout and setting .fonts-failed only after failure
+I cannot produce full FOIT effect till the end of fontobservers timeout.
+In reality, w/o `font-display` descriptor or `font-display: block` I get
+sequence of:
+FOIT > FOUT > styled text
+*/
+html.classList.add('wfonts-inactive');
 
 // inject fontfaceobserver
 const script = document.createElement('script');
@@ -9,19 +19,16 @@ script.async = true;
 script.onload = function () {
   const roboto = new FontFaceObserver('Roboto');
   const sansita = new FontFaceObserver('Sansita');
-  const timeout = 2000;
+  const timeout = 1000;
 
   Promise.all([
     roboto.load(null, timeout),
     sansita.load(null, timeout)
   ])
     .then(function () {
-      html.classList.add('fonts-loaded');
-    })
-    .catch(function () {
-      html.classList.add('fonts-failed');
+      html.classList.remove('wfonts-inactive');
+      html.classList.add('wfonts-active');
     })
 }
 
-// html.appendChild(script);
 document.head.appendChild(script);
