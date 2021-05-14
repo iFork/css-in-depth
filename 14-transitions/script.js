284dd5f2292fce269c36941d6ae70d852dfd1280
Mher <tools@logreg.e4ward.com>
2021-05-14 18:40:53 +0400

    Transition to auto height
    Change height of dropdown drawer to 0 when closed.
    Set overflow to hidden since default overflow is visible canceling the
    effect of 0 height.
    
    `height: auto` is not a valid endpoint for height transition,
    set drawer height when open to a calculated number with JavaScript.
    Use node.scrollHeight prop and set rule w/ node.style.setProperty().
    Use parent.querySelector() to get specific sub-elements.

--- 14-transitions/script.js
+++ 14-transitions/script.js
@@ -1,4 +1,4 @@
-(function () {
+(function registerDropdownTogglers() {
   const dropdownToggles = document.getElementsByClassName('dropdown__toggle');
   Array.prototype.forEach.call(dropdownToggles, function (toggle) {
     const dropdown = toggle.parentNode;
@@ -6,5 +6,25 @@
       dropdown.classList.toggle('is-open');
       console.log({toggle});
     })
+  })
+})();
+(function setDropdownDrawerHeights() {
+  // TODO: refactor - a new func combinng this func w/ registerDropdownTogglers
+  const dropdowns = document.getElementsByClassName('dropdown');
+  Array.prototype.forEach.call(dropdowns, function (dd) {
+    const toggle = dd.querySelector('.dropdown__toggle');
+    const drawer = dd.querySelector('.dropdown__drawer');
+    // TODO:IDEA: consider mutation observer instead of sibling click listening
+    toggle.addEventListener('click',
+      function () { 
+        const height = drawer.scrollHeight;
+        if (dd.classList.contains('is-open')) {
+          drawer.style.setProperty('height', `${height}px`);
+          // console.log('after', drawer.style.getPropertyValue('height'));
+        } else {
+          drawer.style.setProperty('height', 0);
+        }
+      }
+    )
   })
 })();
