033a034ebd2f17a66ad5adfc9244a5455795fd28
Mher <tools@logreg.e4ward.com>
2021-05-29 22:20:59 +0400

    Disable form inputs when loading

--- 16-animations/script.js
+++ 16-animations/script.js
@@ -1,11 +1,17 @@
 
-(function addButtonLoadingState () {
-  const buttons = document.getElementsByClassName('button--submit');
-  Array.prototype.forEach.call(buttons, function (btn) {
+(function formLoadingState () {
+  const forms = document.getElementsByClassName('form');
+  Array.prototype.forEach.call(forms, function (frm) {
+    const inputs = frm.querySelectorAll('textarea, input');
+    const btn = frm.querySelector('.button--submit');
+
     btn.addEventListener('click', function (evt) {
       evt.preventDefault();
       btn.classList.add('is-loading');
       btn.disabled = true;
+      Array.prototype.forEach.call(inputs, function (inp) {
+        inp.disabled = true;
+      })
     })
   })
 })();
