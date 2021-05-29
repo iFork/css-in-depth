073f6f69c163f56768ef7965284c7b6b0caf271a
Mher <tools@logreg.e4ward.com>
2021-05-29 22:43:21 +0400

    Refactor registerFormsLoadingState

--- 16-animations/script.js
+++ 16-animations/script.js
@@ -1,18 +1,22 @@
 
-(function formLoadingState () {
+(function registerFormsLoadingState () {
   const forms = document.getElementsByClassName('form');
-  Array.prototype.forEach.call(forms, function (frm) {
+  Array.prototype.forEach.call(forms, registerFormLoadingState)
+
+  function registerFormLoadingState(frm) {
     const inputs = frm.querySelectorAll('textarea, input');
     const btn = frm.querySelector('.button--submit');
-
     btn.addEventListener('click', function (evt) {
       evt.preventDefault();
       btn.classList.add('is-loading');
       btn.disabled = true;
-      Array.prototype.forEach.call(inputs, function (inp) {
-        inp.disabled = true;
-      })
-    })
-  })
+      disableInputs(inputs);
+    });
+  }
+  function disableInputs(inputs) {
+    Array.prototype.forEach.call(inputs, function (inp) {
+      inp.disabled = true;
+    });
+  }
 })();
 
